19ad2e172af17a1e9deb437745e88376bd4533c9
HEMAHONEY09 <bharathishivu26@gmail.com>
2024-09-30 20:46:49 +0530

    intro of python

---
+++ activity1.py
@@ -0,0 +1,7 @@
+print("Hello world")
+
+print("My name is Anushree")
+
+print("I love playing soccer")
+
+print("the result of 3 + 5 is:", 3 + 5)
---
+++ activity3.py
@@ -0,0 +1,11 @@
+print("Welcome to the world of programming")
+
+
+
+print(4)
+
+print("Hello python \n")
+
+print("Keywords in python are....")
+import keyword
+print(keyword.kwlist)
